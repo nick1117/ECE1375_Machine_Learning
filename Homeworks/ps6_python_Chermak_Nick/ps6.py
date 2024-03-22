01f00d1da7fc23970596f4b07490391b5c3b7af3
Nick Chermak <nac136@pitt.edu>
2024-03-21 22:59:30 -0400

    Max likelihood and Discriminant function for classification

--- Homeworks/ps6_python_Chermak_Nick/ps6.py
+++ Homeworks/ps6_python_Chermak_Nick/ps6.py
@@ -27,18 +27,6 @@ print(f'The shape of X_train_3 is {X_train_3.shape}')
 
 #Question 1
 #Part a
-mean_std = {'Class 1': {'mean': [], 'std': []},
-            'Class 2': {'mean': [], 'std': []},
-            'Class 3': {'mean': [], 'std': []}}
-
-for i in range(cols):
-    mean_std['Class 1']['mean'].append(np.mean(X_train_1[:, i]))
-    mean_std['Class 1']['std'].append(np.std(X_train_1[:, i]))
-    mean_std['Class 2']['mean'].append(np.mean(X_train_2[:, i]))
-    mean_std['Class 2']['std'].append(np.std(X_train_2[:, i]))
-    mean_std['Class 3']['mean'].append(np.mean(X_train_3[:, i]))
-    mean_std['Class 3']['std'].append(np.std(X_train_3[:, i]))
-
 mean_values = []
 std_values = []
 for i in range(cols):
@@ -78,6 +66,53 @@ for entry in X_test:
 
 #Part c
 accuracy = accuracy_score(Y_test, class_predictions)
-print(f'The accuracy of the classifier is: {accuracy*100} %')
+print(f'The accuracy of the Naive-Bayes classifier is: {accuracy*100} %')
 
 #Question 2
+#Part a
+Sigma_1 = np.cov(X_train_1.T)
+Sigma_2 = np.cov(X_train_2.T)
+Sigma_3 = np.cov(X_train_3.T)
+print(f'The size of covariance matrix 1: {Sigma_1.shape}')
+print(f'The size of covariance matrix 2: {Sigma_2.shape}')
+print(f'The size of covariance matrix 3: {Sigma_3.shape}')
+print('Class 1 covariance:')
+print(Sigma_1)
+print('Class 2 covariance:')
+print(Sigma_2)
+print('Class 3 covariance:')
+print(Sigma_3)
+
+#Part b
+mean_1 = np.array(mean_values)[:, 0]
+mean_2 = np.array(mean_values)[:, 1]
+mean_3 = np.array(mean_values)[:, 2]
+print(f'The size of mean 1 vector is: {mean_1.shape}')
+print(f'The size of mean 2 vector is: {mean_2.shape}')
+print(f'The size of mean 3 vector is: {mean_3.shape}')
+print('Class 1 mean vector: ')
+print(mean_1)
+print('Class 2 mean vector: ')
+print(mean_2)
+print('Class 3 mean vector: ')
+print(mean_3)
+
+#Part c
+#need or will not work
+mean_1 = mean_1.reshape(len(mean_1),1)
+mean_2 = mean_2.reshape(len(mean_2),1)
+mean_3 = mean_3.reshape(len(mean_3),1)
+
+class_prediction_Q2 = []
+for entry in X_test:
+    x = entry.reshape(len(entry), 1)
+    g1 = -0.5 * (x - mean_1).T @ np.linalg.inv(Sigma_1) @ (x - mean_1) + np.log(1/3) - (len(entry)/2) * np.log(2 * np.pi) - (0.5) * np.log(np.linalg.det(Sigma_1))
+    g2 = -0.5 * (x - mean_2).T @ np.linalg.inv(Sigma_2) @ (x - mean_2) + np.log(1/3) - (len(entry)/2) * np.log(2 * np.pi) - (0.5) * np.log(np.linalg.det(Sigma_2))
+    g3 = -0.5 * (x - mean_3).T @ np.linalg.inv(Sigma_3) @ (x - mean_3) + np.log(1/3) - (len(entry)/2) * np.log(2 * np.pi) - (0.5) * np.log(np.linalg.det(Sigma_3))
+    g = [g1, g2, g3]
+    #print(g)
+    predicted_class = np.argmax(g) + 1
+    class_prediction_Q2.append(predicted_class)
+    
+accuracy_Q2 = accuracy_score(Y_test, class_prediction_Q2)
+print(f'The accuracy of using MLE and Discriminant function for classification is: {accuracy_Q2 * 100} %')
